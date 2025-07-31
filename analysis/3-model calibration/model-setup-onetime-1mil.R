1e9efe7b5a5408082fd951ba141f73f017c89a56
Hailey Park <haileypark@berkeley.edu>
2025-07-31 13:14:43 -0700

    editing calibration scripts

--- analysis/3-model calibration/model-setup-onetime-1mil.R
+++ analysis/3-model calibration/model-setup-onetime-1mil.R
@@ -16,9 +16,12 @@ weekly_severe_incidence <- read.csv("data/clean-data/weekly-incidence-estimates-
                                TRUE ~ age_group)) 
 
 #Ratios between age-specific case hospitalization fractions
-# NOTE: 1. The baseline case-hospitalization fraction `case_hosp_frac` is a calibrated parameter that sets the relationship between
-#          severe and non-severe cases.
-#       2. The case-hospitalzrelationship between all the The reference 50-64 years
+# NOTE: 1. These are age-specific case hospitalization fractions from Herrera-Esposito D, et. al (BMC Infec Dis., 2022)
+#       2. This dataframe creates fixed ratios between the case-hospitalization fractions. The baseline group for setting the ratio is the 50-64 years.
+#       3. Since these case-hospitalization fractions are dated, we calibrate a parameter "baseline case-hospitalization fraction" to sets the relationship
+#          between severe and non-severe cases.
+#       4. Link: https://pmc.ncbi.nlm.nih.gov/articles/PMC8962942/
+         
 age_ratio_case_hosp_frac <- data.frame(age_group = c("0-17 years", "18-29 years", "30-49 years", "50-64 years", "65-74 years", "75+ years"),
                                        case_hosp_frac = c(0.155, 0.4283, 1.545, 5.9, 13, 30)) %>%
   mutate(ratio = case_hosp_frac/5.9)
@@ -32,7 +35,7 @@ waning_data_clean_alt <- setDT(read.csv("data/clean-data/waning_data_clean_alt.c
   filter(weeks %% 2 == 0) %>% mutate(weeks = weeks/2)
 setkeyv(waning_data_clean_alt, c("immuno", "weeks", "prior_inf", "prior_vacc"))
 
-#Calculating total current infections at model initialization, based on average severe and nonsevere incidence estimates at July 2023
+#Calculating average severe incidence around model initialization (July 2023), which will be used later to estimate total circulating infections to feed into the model
 average_severe_incidence <- melt(weekly_severe_incidence %>% filter(weeks_since < 4) %>% group_by(age_group) %>% 
   summarise(across(healthy_inc:higher_risk_inc, mean)), id = "age_group") %>% 
   rowwise() %>% dplyr::mutate(risk_group = strsplit(as.character(variable), split="_")[[1]][1],
@@ -45,6 +48,6 @@ entire_pop <-  read.csv(paste0("data/clean-data/entire_population_model_initiali
 #Read vaccine-uptake-assignment script and run vaccine assignment based on historical coverage on entire population
 source(here::here("vaccine-uptake-assignment-calibration.R"))
 
-entire_pop_with_vax_assigned <- realistic_vax_assignment(entire_pop)
+entire_pop_with_vax_assigned <- historical_vax_assignment(entire_pop)
 
 ############################################################################################
